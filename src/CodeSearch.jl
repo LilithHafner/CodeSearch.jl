396fb95e34a64e015656311b8f881e135990facc
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-02-17 07:37:30 -0600

    Add gen_unused_symbol

--- src/CodeSearch.jl
+++ src/CodeSearch.jl
@@ -1,5 +1,37 @@
 module CodeSearch
 
-# Write your package code here.
+get_symbols!(symbols, expr::Symbol) = push!(symbols, expr)
+function get_symbols!(symbols, expr::Expr)
+    for arg in expr.args
+        get_symbols!(symbols, arg)
+    end
+    symbols
+end
+get_symbols!(symbols, expr::Any) = symbols
+get_unique_symbols(expr) = get_symbols!(Set{Symbol}(), expr)
+
+"""
+    gen_unused_symbol(expr, prefix=:hole)
+
+Generate a symbol with the given `prefix` that does not occur in `expr`.
+
+Tries, in order:
+1. `prefix`
+2. `prefix`1
+3. `prefix`2
+4. ...
+"""
+function gen_unused_symbol(expr, prefix=:hole)
+    symbols = get_unique_symbols(expr)
+    if prefix in symbols
+        i = 1
+        while Symbol(prefix, i) in symbols
+            i += 1
+        end
+        Symbol(prefix, i)
+    else
+        prefix
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,37 +8,49 @@ using Aqua
         Aqua.test_deps_compat(CodeSearch, check_extras=false)
     end
 
-    haystack = """
-    function f()
-        println("Hello, world!")
+    @testset "occursin" begin
+        haystack = """
+        function f()
+            println("Hello, world!")
+        end
+
+        while true
+            f()
+        end
+
+        if false
+            f()
+            g()
+        end
+
+        1+1 # Comments
+
+        for _ in 1:
+        """
+
+        @test occursin(j"while *; f(); end", haystack)
+        @test !occursin(j"while *; g(); end", haystack)
+        @test occursin(j"while *; *; end", haystack)
+        @test occursin(j"if false; *; end", haystack)
+        @test !occursin(j"while * end", haystack)
+        @test !occursin(j"if *
+                            g()
+                        end", haystack)
+        @test !occursin(j"if *
+                            f()
+                            g()
+                        end", haystack)
+        @test occursin(j"1 + 1 # Comments", haystack)
+        @test_broken !occursin(j"1 + 1 # Conts", haystack)
     end
 
-    while true
-        f()
+    @testset "gen_unused_symbol" begin
+        @test CodeSearch.gen_unused_symbol(:(ho = le)) === :hole
+        @test CodeSearch.gen_unused_symbol(:(hole + 1)) === :hole1
+        @test CodeSearch.gen_unused_symbol(:(hole + hole1)) === :hole2
+        @test CodeSearch.gen_unused_symbol(:(hole + hole10)) === :hole1
+        @test CodeSearch.gen_unused_symbol(
+                :(hole, hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10)
+            ) === :hole11
     end
-
-    if false
-        f()
-        g()
-    end
-
-    1+1 # Comments
-
-    for _ in 1:
-    """
-
-    @test occursin(j"while *; f(); end", haystack)
-    @test !occursin(j"while *; g(); end", haystack)
-    @test occursin(j"while *; *; end", haystack)
-    @test occursin(j"if false; *; end", haystack)
-    @test !occursin(j"while * end", haystack)
-    @test !occursin(j"if *
-                        g()
-                      end", haystack)
-    @test !occursin(j"if *
-                        f()
-                        g()
-                      end", haystack)
-    @test occursin(j"1 + 1 # Comments", haystack)
-    @test_broken !occursin(j"1 + 1 # Conts", haystack)
 end
