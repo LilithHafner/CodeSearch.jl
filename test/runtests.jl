2f4fc5d51a8da3237ed8f7f1faeefd193ab34972
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-02-17 07:14:34 -0600

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,38 @@ using Aqua
         Aqua.test_all(CodeSearch, deps_compat=false)
         Aqua.test_deps_compat(CodeSearch, check_extras=false)
     end
-    # Write your tests here.
+
+    haystack = """
+    function f()
+        println("Hello, world!")
+    end
+
+    while true
+        f()
+    end
+
+    if false
+        f()
+        g()
+    end
+
+    1+1 # Comments
+
+    for _ in 1:
+    """
+
+    @test occursin(j"while *; f(); end", haystack)
+    @test !occursin(j"while *; g(); end", haystack)
+    @test occursin(j"while *; *; end", haystack)
+    @test occursin(j"if false; *; end", haystack)
+    @test !occursin(j"while * end", haystack)
+    @test !occursin(j"if *
+                        g()
+                      end", haystack)
+    @test !occursin(j"if *
+                        f()
+                        g()
+                      end", haystack)
+    @test occursin(j"1 + 1 # Comments", haystack)
+    @test_broken !occursin(j"1 + 1 # Conts", haystack)
 end
